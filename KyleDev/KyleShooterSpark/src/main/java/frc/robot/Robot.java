ef7a933c54430cbce3b65a22acb56ca076b9bd10
Robotics-SL07 <team3176@gmail.com>
2020-01-18 13:53:55 -0500

    switch 1/18/20

--- KyleDev/KyleShooterSpark/src/main/java/frc/robot/Robot.java
+++ KyleDev/KyleShooterSpark/src/main/java/frc/robot/Robot.java
@@ -12,16 +12,12 @@ import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj.Joystick;
 
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
-/**
- * The VM is configured to automatically run this class, and to call the
- * functions corresponding to each mode, as described in the TimedRobot
- * documentation. If you change the name of this class or the package after
- * creating this project, you must also update the build.gradle file in the
- * project.
- */
+
 public class Robot extends TimedRobot {
   private static final String kDefaultAuto = "Default";
   private static final String kCustomAuto = "My Auto";
@@ -30,13 +26,16 @@ public class Robot extends TimedRobot {
 
   public Joystick left = new Joystick(0);
   public Joystick right = new Joystick(1);
+  public Joystick middle = new Joystick(1);
   private CANSparkMax LMotor = new CANSparkMax(4, MotorType.kBrushless);
   private CANSparkMax RMotor = new CANSparkMax(1, MotorType.kBrushless);
+  private TalonSRX conveyor = new TalonSRX(44);
   private double lpct;
   private double rpct;
   private double lvelocity;
   private double rvelocity;
-   
+  private double convpct;
+  private double convvelocity;
 
 
   /**
@@ -48,71 +47,26 @@ public class Robot extends TimedRobot {
     m_chooser.setDefaultOption("Default Auto", kDefaultAuto);
     m_chooser.addOption("My Auto", kCustomAuto);
     SmartDashboard.putData("Auto choices", m_chooser);
-    RMotor.getEncoder().setVelocityConversionFactor(1);
-    LMotor.getEncoder().setVelocityConversionFactor(1);
-
-    
+    RMotor.getEncoder().setVelocityConversionFactor(1.0);
+    LMotor.getEncoder().setVelocityConversionFactor(1.0);
   }
-
-  /**
-   * This function is called every robot packet, no matter the mode. Use
-   * this for items like diagnostics that you want ran during disabled,
-   * autonomous, teleoperated and test.
-   *
-   * <p>This runs after the mode specific periodic functions, but before
-   * LiveWindow and SmartDashboard integrated updating.
-   */
-  @Override
-  public void robotPeriodic() {
-  }
-
-  /**
-   * This autonomous (along with the chooser code above) shows how to select
-   * between different autonomous modes using the dashboard. The sendable
-   * chooser code works with the Java SmartDashboard. If you prefer the
-   * LabVIEW Dashboard, remove all of the chooser code and uncomment the
-   * getString line to get the auto name from the text box below the Gyro
-   *
-   * <p>You can add additional auto modes by adding additional comparisons to
-   * the switch structure below with additional strings. If using the
-   * SendableChooser make sure to add them to the chooser code above as well.
-   */
   @Override
   public void autonomousInit() {
     m_autoSelected = m_chooser.getSelected();
     // m_autoSelected = SmartDashboard.getString("Auto Selector", kDefaultAuto);
     System.out.println("Auto selected: " + m_autoSelected);
   }
-
-  /**
-   * This function is called periodically during autonomous.
-   */
-  @Override
-  public void autonomousPeriodic() {
-    switch (m_autoSelected) {
-      case kCustomAuto:
-        // Put custom auto code here
-        break;
-      case kDefaultAuto:
-      default:
-        // Put default auto code here
-        break;
-    }
-  }
-
-  /**
-   * This function is called periodically during operator control.
-   */
+  
   @Override
   public void teleopPeriodic() {
     
-    /*if(XboxController.getY() >= 0.1 || XboxController.getY() <= -0.1) {
-      
-    }*/
-    lpct = left.getRawAxis(3)/-2.0+0.5;
-    rpct = right.getRawAxis(3)/-2.0+0.5;
+    lpct = (left.getRawAxis(3)/-2.0+0.5);
+    rpct = (right.getRawAxis(3)/-2.0+0.5);
+    convpct = (middle.getRawAxis(3)/-2.0+0.5);
     RMotor.set(rpct);
     LMotor.set(lpct);
+    conveyor.set(ControlMode.PercentOutput, convpct);
+    
     
     
     lvelocity=LMotor.getEncoder().getVelocity();
@@ -120,13 +74,12 @@ public class Robot extends TimedRobot {
 
     SmartDashboard.putNumber("L%: ", lpct);
     SmartDashboard.putNumber("R%: ", rpct);
+    SmartDashboard.putNumber("Tread %", convpct);
     SmartDashboard.putNumber("LVel: ", lvelocity);
     SmartDashboard.putNumber("RVel: ", rvelocity);
     SmartDashboard.putNumber("LVel Graph: ", lvelocity);
     SmartDashboard.putNumber("RVel Graph: ", rvelocity);
-   }
-   
-  @Override
-  public void testPeriodic() {
+    
   }
 }
+
