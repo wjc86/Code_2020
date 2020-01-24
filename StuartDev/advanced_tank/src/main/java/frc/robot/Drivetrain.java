5cf82d85018c702a2d7cb243d7b76381a9a600ac
Kyle <45471789+kphillips2003@users.noreply.github.com>
2020-01-23 19:36:21 -0500

    1/23

--- StuartDev/advanced_tank/src/main/java/frc/robot/Drivetrain.java
+++ StuartDev/advanced_tank/src/main/java/frc/robot/Drivetrain.java
@@ -7,9 +7,31 @@
 
 package frc.robot;
 
+import edu.wpi.first.wpilibj.SpeedController;
+import edu.wpi.first.wpilibj.SpeedControllerGroup;
+import edu.wpi.first.wpilibj.controller.PIDController;
+import edu.wpi.first.wpilibj.controller.SimpleMotorFeedforward;
+import edu.wpi.first.wpilibj.geometry.Rotation2d;
+import edu.wpi.first.wpilibj.kinematics.ChassisSpeeds;
+import edu.wpi.first.wpilibj.kinematics.DifferentialDriveKinematics;
+import edu.wpi.first.wpilibj.kinematics.DifferentialDriveOdometry;
+import edu.wpi.first.wpilibj.kinematics.DifferentialDriveWheelSpeeds;
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
+import com.kauailabs.navx.frc.AHRS;
+import frc.robot.Constants;
+
 
 /**
  * Add your docs here.
  */
 public class Drivetrain {
+    private final WPI_TalonFX m_leftMaster = new WPI_TalonFX(Constants.leftMasterID);
+    private final WPI_TalonFX m_leftFollower = new WPI_TalonFX(Constants.leftFollowerID);
+    private final WPI_TalonFX m_rightMaster = new WPI_TalonFX(Constants.rightMasterID);
+    private final WPI_TalonFX m_rightFollower = new WPI_TalonFX(Constants.rightFollowerID);
+
+    private final AHRS m_gyro = new AHRS(SPI.Port.kMXP);
+
+    SimpleMotorFeedforward feedforward = new SimpleMotorFeedforward(ks, kv)
+
 }
