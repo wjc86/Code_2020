c6989d4ac521e826823c1af6eba36c3f130c0c48
Robotics-SL07 <team3176@gmail.com>
2020-02-03 20:52:50 -0500

    Turret 2/3/20

--- KyleDev/Newpractice/src/main/java/frc/robot/controller.java
+++ KyleDev/Newpractice/src/main/java/frc/robot/controller.java
@@ -1,12 +1,7 @@
 package frc.robot;
 
 import edu.wpi.first.wpilibj.Joystick;
-
 public class controller{
-    Joystick leftStick;
-    Joystick rightStick;
-    public controller()
-    {
-
-    }
+    public static Joystick leftStick = new Joystick(0);
+    public static Joystick rightStick = new Joystick(1);
 }
--- KyleDev/Turret/src/main/java/frc/robot/Robot.java
+++ KyleDev/Turret/src/main/java/frc/robot/Robot.java
@@ -10,12 +10,7 @@ package frc.robot;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-import edu.wpi.first.wpilibj.Joystick;
 
-import com.ctre.phoenix.motorcontrol.ControlMode;
-import com.ctre.phoenix.motorcontrol.can.TalonFX;
-import frc.robot.constants;
-import frc.robot.controller;
 /**
  * The VM is configured to automatically run this class, and to call the
  * functions corresponding to each mode, as described in the TimedRobot
@@ -28,8 +23,7 @@ public class Robot extends TimedRobot {
   private static final String kCustomAuto = "My Auto";
   private String m_autoSelected;
   private final SendableChooser<String> m_chooser = new SendableChooser<>();
-  TalonFX leftMotor = new TalonFX(1);
-  TalonFX rightMotor = new TalonFX(2);
+
   /**
    * This function is run when the robot is first started up and should be
    * used for any initialization code.
@@ -39,7 +33,6 @@ public class Robot extends TimedRobot {
     m_chooser.setDefaultOption("Default Auto", kDefaultAuto);
     m_chooser.addOption("My Auto", kCustomAuto);
     SmartDashboard.putData("Auto choices", m_chooser);
-    rightMotor.setInverted(true);  
   }
 
   /**
@@ -93,17 +86,6 @@ public class Robot extends TimedRobot {
    */
   @Override
   public void teleopPeriodic() {
-    if(Math.abs(controller.leftStick.getY()) < constants.DEADBAND) {
-      leftMotor.set(ControlMode.PercentOutput, 0);
-    }else{
-      leftMotor.set(ControlMode.PercentOutput, controller.leftStick.getY());
-    }
-
-    if(Math.abs(controller.rightStick.getY()) < constants.DEADBAND) {
-      rightMotor.set(ControlMode.PercentOutput, 0);
-    }else{
-      rightMotor.set(ControlMode.PercentOutput, controller.rightStick.getY());
-    }
   }
 
   /**
@@ -113,3 +95,14 @@ public class Robot extends TimedRobot {
   public void testPeriodic() {
   }
 }
+
+/*
+2 Limit Switches
+Spin Limit
+Vision Integration
+Motor (SparkMax or Falcon 500) with encoder value to determine angle
+if reached enpoint speeed/pct = -speed/pct
+switch case
+eStop
+
+*/
