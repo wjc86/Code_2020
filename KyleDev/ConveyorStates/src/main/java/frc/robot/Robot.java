4112fb766b037fce7192c2c5320823187d511c3c
Robotics-SL07 <team3176@gmail.com>
2020-02-06 20:48:47 -0500

    Conveyor States 2/6/20 update

--- KyleDev/ConveyorStates/src/main/java/frc/robot/Robot.java
+++ KyleDev/ConveyorStates/src/main/java/frc/robot/Robot.java
@@ -6,7 +6,8 @@ import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj.Joystick;
 import com.revrobotics.CANSparkMax;
-import com.revrobotics.CANSparkMaxLowLevel.MotorType;;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+import edu.wpi.first.wpilibj.util.Units;
 
 public class Robot extends TimedRobot {
   private static final String kDefaultAuto = "Default";
@@ -15,76 +16,56 @@ public class Robot extends TimedRobot {
   private final SendableChooser<String> m_chooser = new SendableChooser<>();
   public Joystick controller = new Joystick(1);
   public CANSparkMax upperMotor = new CANSparkMax(1, MotorType.kBrushless);
-  /**
-   * This function is run when the robot is first started up and should be
-   * used for any initialization code.
-   */
+  
   @Override
   public void robotInit() {
     m_chooser.setDefaultOption("Default Auto", kDefaultAuto);
     m_chooser.addOption("My Auto", kCustomAuto);
-    SmartDashboard.putData("Auto choices", m_chooser);
+
+    upperMotor.getEncoder().setVelocityConversionFactor(1.0/60.0/8.75*Units.inchesToMeters(2)*Math.PI);
   }
 
-  /**
-   * This function is called every robot packet, no matter the mode. Use
-   * this for items like diagnostics that you want ran during disabled,
-   * autonomous, teleoperated and test.
-   *
-   * <p>This runs after the mode specific periodic functions, but before
-   * LiveWindow and SmartDashboard integrated updating.
-   */
+  
   @Override
   public void robotPeriodic() {
   }
 
-  /**
-   * This autonomous (along with the chooser code above) shows how to select
-   * between different autonomous modes using the dashboard. The sendable
-   * chooser code works with the Java SmartDashboard. If you prefer the
-   * LabVIEW Dashboard, remove all of the chooser code and uncomment the
-   * getString line to get the auto name from the text box below the Gyro
-   *
-   * <p>You can add additional auto modes by adding additional comparisons to
-   * the switch structure below with additional strings. If using the
-   * SendableChooser make sure to add them to the chooser code above as well.
-   */
+
   @Override
   public void autonomousInit() {
     m_autoSelected = m_chooser.getSelected();
-    // m_autoSelected = SmartDashboard.getString("Auto Selector", kDefaultAuto);
+   
     System.out.println("Auto selected: " + m_autoSelected);
   }
 
-  /**
-   * This function is called periodically during autonomous.
-   */
+  
   @Override
   public void autonomousPeriodic() {
     switch (m_autoSelected) {
       case kCustomAuto:
-        // Put custom auto code here
+     
         break;
       case kDefaultAuto:
       default:
-        // Put default auto code here
+      
         break;
     }
   }
 
-  /**
-   * This function is called periodically during operator control.
-   */
+  
   @Override
   public void teleopPeriodic() {
       if (controller.getRawButton(1)) {
           upperMotor.set(0.5);
       }
+      else {
+        upperMotor.set(0);
+         
+      }
+    SmartDashboard.putNumber("speed", upperMotor.getEncoder().getVelocity());
   }
 
-  /**
-   * This function is called periodically during test mode.
-   */
+ 
   @Override
   public void testPeriodic() {
   }
