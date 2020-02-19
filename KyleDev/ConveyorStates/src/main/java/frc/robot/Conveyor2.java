cbd01ab1c1757a12021b3d37a6a3da1ac990699e
Kyle <45471789+kphillips2003@users.noreply.github.com>
2020-02-18 20:51:07 -0500

    ConveyorStates Competitoin Ver. update 2/18/20

--- KyleDev/ConveyorStates/src/main/java/frc/robot/Conveyor2.java
+++ KyleDev/ConveyorStates/src/main/java/frc/robot/Conveyor2.java
@@ -1,15 +1,18 @@
 package frc.robot;
 
+import com.revrobotics.CANEncoder;
 import com.revrobotics.CANPIDController;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.ControlType;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
+import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class Conveyor2 {
     CANSparkMax conveyorMotor = new CANSparkMax(1, MotorType.kBrushless);
     CANPIDController conveyorPID = conveyorMotor.getPIDController();
+    CANEncoder conveyorEncoder = conveyorMotor.getEncoder();
     double[] K = new double[3]; //Array of gains
     double[] K_UI = new double[3];
     double beltDelta; //length in inches
@@ -18,8 +21,12 @@ public class Conveyor2 {
     double distancePerRot = 7.0037; // inches per revolution
     // Boolean isStaged = false;
     // int state = 0;
+    DigitalInput sensor1 = new DigitalInput(0);
+    DigitalInput sensor2 = new DigitalInput(2);
     
     public Conveyor2(){
+        conveyorMotor.restoreFactoryDefaults();
+        conveyorEncoder.setPosition(0);
         this.getData();
         this.pushData();
         SmartDashboard.putNumber("P Gain", 0);
@@ -45,10 +52,14 @@ public class Conveyor2 {
 
     public void setData() {
         this.K = this.K_UI;
-        this.beltDelta += this.beltDelta_ui;
+        this.beltDelta = this.beltDelta_ui;
         conveyorPID.setP(this.K[0]);
         conveyorPID.setI(this.K[1]);
         conveyorPID.setD(this.K[2]);
+        conveyorPID.setIZone(0);
+        conveyorPID.setFF(0);
+        conveyorPID.setOutputRange(-1, 1);
+        
         if(this.beltDelta > 0) {
             this.advanceBelt();
         }
@@ -56,13 +67,18 @@ public class Conveyor2 {
 
     public void pushData() {
         double measure = this.conveyorMotor.getEncoder().getPosition();
+        this.beltPosition = conveyorMotor.getEncoder().getPosition();
+
+        SmartDashboard.putNumber("Reference: ", this.beltPosition/this.distancePerRot + this.beltDelta);
         SmartDashboard.putNumber("Measured Value ", measure);
         SmartDashboard.putNumber("Target Value ", 0);
-        
+        SmartDashboard.putBoolean("Top Line ", sensor1.get()); 
+        SmartDashboard.putBoolean("Bottom Line ", sensor2.get());
     }
 
     public void advanceBelt(){
-        conveyorPID.setReference(this.beltPosition/this.distancePerRot + this.beltDelta, ControlType.kPosition);
+        // conveyorPID.setReference((this.beltPosition + this.beltDelta)/this.distancePerRot, ControlType.kPosition);
+        conveyorPID.setReference(this.beltDelta/this.distancePerRot, ControlType.kPosition);
         this.beltDelta = 0;
     }
     /*public void stateTransfer() {
