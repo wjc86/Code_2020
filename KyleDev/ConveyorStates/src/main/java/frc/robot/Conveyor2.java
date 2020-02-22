cb3f1f4740f081766342e920fd672fafecaf9c32
Kyle <45471789+kphillips2003@users.noreply.github.com>
2020-02-21 20:51:58 -0500

    Conveyor Update 2/21

--- KyleDev/ConveyorStates/src/main/java/frc/robot/Conveyor2.java
+++ KyleDev/ConveyorStates/src/main/java/frc/robot/Conveyor2.java
@@ -13,13 +13,12 @@ public class Conveyor2 {
     public CANSparkMax conveyorMotor;
     public CANPIDController conveyorPID;
     public CANEncoder conveyorEncoder;
-    
-    // double[] K = new double[3]; //Array of gains
-    // double[] K_UI = new double[3];
     double beltDelta; //length in inches
-    double beltPosition;
+    double beltPosition, beltDelta_ui;
     double distancePerRot = 7.0037; // inches per revolution
-    double p, i, d;
+    double p = 0.08;
+    double i = 0.075;
+    double d = 0.07;
 
     DigitalInput sensor1 = new DigitalInput(0);
     DigitalInput sensor2 = new DigitalInput(2);
@@ -29,46 +28,21 @@ public class Conveyor2 {
         conveyorMotor.restoreFactoryDefaults();
         conveyorPID = conveyorMotor.getPIDController();
         conveyorEncoder = conveyorMotor.getEncoder();
-        conveyorEncoder.setPosition(0);
+        conveyorEncoder.setPosition(0.0);
         conveyorEncoder.setPositionConversionFactor(1.0);
         this.getData();
         this.pushData();
-        conveyorPID.setP(0);
-        conveyorPID.setI(0);
-        conveyorPID.setD(0);
-        conveyorPID.setIZone(0);
-        conveyorPID.setFF(0);
-        conveyorPID.setOutputRange(-1, 1);
-
-        SmartDashboard.putNumber("P Gain", 0);
-        SmartDashboard.putNumber("I Gain", 0);
-        SmartDashboard.putNumber("D Gain", 0);
-        SmartDashboard.putNumber("Set Belt Delta", 0);
-    }
-    
-    public void getData() {
-        this.p = SmartDashboard.getNumber("P Gain", 0);
-        this.i = SmartDashboard.getNumber("I Gain", 0);
-        this.d = SmartDashboard.getNumber("D Gain", 0);
-        this.beltDelta = SmartDashboard.getNumber("Set Belt Delta", 0);
-        // this.K_UI[0] = p;
-        // this.K_UI[1] = i;
-        // this.K_UI[2] = d;
-        conveyorMotor.set(0);
-    }
-
-    public void home(){
-        conveyorPID.setReference(0, ControlType.kPosition);
-    }
-
-    public void setData() {
-        // this.K = this.K_UI;
         conveyorPID.setP(p);
         conveyorPID.setI(i);
         conveyorPID.setD(d);
-        // conveyorPID.setP(this.K[0]);
-        // conveyorPID.setI(this.K[1]);
-        // conveyorPID.setD(this.K[2]);
+    }
+    
+    public void getData() {
+        this.beltDelta_ui = SmartDashboard.getNumber("Set Belt Delta", 0);
+    }
+
+    public void setData() {
+        this.beltDelta = this.beltDelta_ui;
     }
 
     public void pushData() {
@@ -82,7 +56,10 @@ public class Conveyor2 {
     }
 
     public void advanceBelt(){
-        conveyorPID.setReference(this.beltDelta/this.distancePerRot, ControlType.kPosition);
-        // this.beltDelta = 0;
+        conveyorPID.setReference(this.beltDelta/this.distancePerRot+this.beltPosition, ControlType.kPosition);
+        double reference = this.beltDelta/this.distancePerRot;
+        SmartDashboard.putNumber("Set Reference", reference);
+        SmartDashboard.putNumber("Encoder Position", conveyorEncoder.getPosition());
+        this.beltDelta = 0;
     }
 }
