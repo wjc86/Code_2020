377a93a678254860e3a4780a61b130dd9f80f36d
Jared Brown <jaredab370@gmail.com>
2020-02-22 11:56:28 -0500

    SpinUpFlywheel started

--- JaredDev/FlywheelSpinUp/src/main/java/frc/robot/RobotContainer.java
+++ JaredDev/FlywheelSpinUp/src/main/java/frc/robot/RobotContainer.java
@@ -9,6 +9,8 @@ package frc.robot;
 
 import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.XboxController;
+import frc.robot.commands.ExampleCommand;
+import frc.robot.subsystems.ExampleSubsystem;
 import edu.wpi.first.wpilibj2.command.Command;
 
 /**
@@ -19,7 +21,11 @@ import edu.wpi.first.wpilibj2.command.Command;
  */
 public class RobotContainer {
   // The robot's subsystems and commands are defined here...
-  
+  private final ExampleSubsystem m_exampleSubsystem = new ExampleSubsystem();
+
+  private final ExampleCommand m_autoCommand = new ExampleCommand(m_exampleSubsystem);
+
+
 
   /**
    * The container for the robot.  Contains subsystems, OI devices, and commands.
